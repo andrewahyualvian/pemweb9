9ac39953e69b116301e4d41df61315798a00b3af
andrewahyu <145315786+andrewahyualvian@users.noreply.github.com>
2023-12-16 19:36:13 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# pemweb9
+# Pertemuan_9
+
+Halaman Awal
+![image](https://github.com/Ranggawan09/Pertemuan_9/assets/145315699/05064116-8ed3-4791-ba84-9d67da2bef22)
---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php
+$koneksi = mysqli_connect("localhost","root","","sekolah");
+if (mysqli_connect_error()){
+    echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+?>
